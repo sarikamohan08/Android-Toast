6174104599a837647d4e6203f1f057430047f1d5
sarikamohan08 <sarikamohan08@gmail.com>
2022-06-07 22:15:13 +0530

    initial commit

---
+++ app/src/main/java/com/example/toast/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void helloPressed(View view){
+        //Toast.makeText(this,"OK", Toast.LENGTH_SHORT).show();
+        showCustomToast("customised");
+    }
+    public void showCustomToast(String string){
+        LayoutInflater inflater=getLayoutInflater();
+        View layout=inflater.inflate(R.layout.toast_layout,(ViewGroup) findViewById(R.id.toast_root));
+        TextView toastText=layout.findViewById(R.id.toast_text);
+        toastText.setText(string);
+        Toast toast =new Toast(getApplicationContext());
+        toast.setGravity(Gravity.CENTER,0,0);
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setView(layout);
+        toast.show();
+    }
+}
